0e5b6cef8cbddb9e27c2396817c39b2d3ac326df
Navil Noor <92982456+navil-noor@users.noreply.github.com>
2023-07-13 14:44:50 +0200

    fixed Nonetype error in rescale.py

--- Tutorials/Basic/rescale.py
+++ Tutorials/Basic/rescale.py
@@ -1,8 +1,9 @@
 import cv2 as cv
 
-img = cv.imread('../Photos/bird1.jpg')
-cv.imshow('Bird', img)
+# img = cv.imread('../Photos/bird1.jpg')
+# cv.imshow('Bird', img)
 
+# cv.waitKey(0)
 
 def rescaleFrame(frame, scale=0.25):
     width = int(frame.shape[1] * scale)
@@ -12,14 +13,15 @@ def rescaleFrame(frame, scale=0.25):
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
 
-resized_image = rescaleFrame(img)
-cv.imshow('Image', resized_image)
-
 # Reading Videos
-capture = cv.VideoCapture('Videos/Sunset.mp4')
+capture = cv.VideoCapture('../Videos/Sunset.mp4')
 
 while True:
     isTrue, frame = capture.read()
+
+    if not isTrue:
+        # Break the loop if there are no more frames to read
+        break
 
     frame_resized = rescaleFrame(frame)
 
@@ -28,3 +30,6 @@ while True:
 
     if cv.waitKey(20) & 0xFF == ord('d'):
         break
+
+capture.release()
+cv.destroyAllWindows()
